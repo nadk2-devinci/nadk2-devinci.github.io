10f1fcf2c3c4b9010bd12c7004ebe836225cc1fe
gdalmon <dalmgabi@gmail.com>
2023-12-29 23:42:29 +0100

    Refactor React import to use StrictMode

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import { StrictMode } from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 // Bootstrap CSS
@@ -10,9 +10,9 @@ import reportWebVitals from './reportWebVitals';;
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <StrictMode>
     <App />
-  </React.StrictMode>
+  </StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
